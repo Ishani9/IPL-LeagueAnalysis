636f7f82412b279287d9fbcdf98bcc11554c1d0c
Ishani Mhatre <mhatreishani1999@gmail.com>
2020-10-28 19:21:47 +0530

    Added: Functionality to find top batting averages of the Cricketers

--- src/main/java/com/leagueanalysis/IPLAnalysis.java
+++ src/main/java/com/leagueanalysis/IPLAnalysis.java
@@ -1,5 +1,69 @@
 package com.leagueanalysis;
 
-public class IPLAnalysis {
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
 
+import com.google.gson.Gson;
+
+import gradleAssignment.CSVBuilderException;
+import gradleAssignment.CSVBuilderFactory;
+import gradleAssignment.CSVBuilderInterface;
+
+public class IPLAnalysis {
+	
+	List<CSVRuns> csvRunsList = null;
+	List<CSVWickets> csvWktsList = null;
+
+	public int loadDataOfRuns(String CSVFile) throws IOException, CSVBuilderException {
+		Reader reader = Files.newBufferedReader(Paths.get(CSVFile));
+		@SuppressWarnings("unchecked")
+		CSVBuilderInterface<CSVRuns> csvBuilder = CSVBuilderFactory.createCSVBuilder();
+		csvRunsList = csvBuilder.getCSVFileList(reader, CSVRuns.class);
+		return csvRunsList.size();
+	}
+
+	public int loadDataOfWickets(String CSVFile) throws IOException, CSVBuilderException {
+		Reader reader = Files.newBufferedReader(Paths.get(CSVFile));
+		@SuppressWarnings("unchecked")
+		CSVBuilderInterface<CSVWickets> csvBuilder = CSVBuilderFactory.createCSVBuilder();
+		csvWktsList = csvBuilder.getCSVFileList(reader, CSVWickets.class);
+		return csvWktsList.size();
+	}
+	
+	/**
+	 * UC 1
+	 * 
+	 * @return
+	 * @throws CSVBuilderException
+	 * @throws IOException
+	 */
+	public double getTopBattingAvg() throws CSVBuilderException, IOException {
+		double max = csvRunsList.stream().map(CSVRuns -> CSVRuns.average).max(Double::compare).get();
+		return max;
+	}
+	
+	public String getAverageWiseSortedData() {
+		Comparator<CSVRuns> comparator = Comparator.comparing(entry -> entry.average);
+		this.sort(csvRunsList, comparator);
+		String sorted = new Gson().toJson(csvRunsList);
+		return sorted;
+	}
+
+	public <E> void sort(List<E> csvList, Comparator<E> comparator) {
+		for (int i = 0; i < csvList.size(); i++) {
+			for (int j = 0; j < csvList.size() - i - 1; j++) {
+				E player1 = csvList.get(j);
+				E player2 = csvList.get(j + 1);
+				if (comparator.compare(player1, player2) < 0) {
+					csvList.set(j, player2);
+					csvList.set(j + 1, player1);
+				}
+			}
+		}
+	}
 }
--- src/test/java/com/leagueanalysis/IPLAnalysisTest.java
+++ src/test/java/com/leagueanalysis/IPLAnalysisTest.java
@@ -1,5 +1,91 @@
 package com.leagueanalysis;
 
-public class IPLAnalysisTest {
+import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import gradleAssignment.CSVBuilderException;
+
+public class IPLAnalysisTest {
+	
+	public static String RUNS_FILE = "C:\\Users\\Ishani\\eclipse-workspace\\com.leagueanalysis\\MostRuns";
+	public static String WICKETS_FILE = "C:\\Users\\Ishani\\eclipse-workspace\\com.leagueanalysis\\MostWkts";
+	IPLAnalysis iplAnalysis;
+	
+	@Before
+	public void setUp() {
+		iplAnalysis = new IPLAnalysis();
+	}
+	
+	
+	@Test
+	public void givenFileData_IfMatchNumberOfRecords_ShouldReturnTrue() {
+		try {
+			int count = iplAnalysis.loadDataOfRuns(RUNS_FILE);
+			assertEquals(101, count);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		catch (CSVBuilderException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void givenWKTSFileData_IfMatchNumberOfRecords_ShouldReturnTrue() {
+		try {
+			int count = iplAnalysis.loadDataOfWickets(WICKETS_FILE);
+			assertEquals(99, count);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		catch (CSVBuilderException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * UC 1
+	 * 
+	 * @throws IOException
+	 * @throws CSVBuilderException
+	 */
+	@Test
+	public void givenRunsData_WhenSortedOnAverage_ShouldReturnHighest() throws IOException, CSVBuilderException {
+		try {
+			iplAnalysis.loadDataOfRuns(RUNS_FILE);
+			Double sortedCSVData = iplAnalysis.getTopBattingAvg();
+			Double exp = 83.2;
+			assertEquals(exp, sortedCSVData);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void givenRunsData_WhenSortedOnAverage_ShouldReturnTrue() throws IOException, CSVBuilderException {
+		try {
+			iplAnalysis.loadDataOfRuns(RUNS_FILE);
+			String sortedCSVData = iplAnalysis.getAverageWiseSortedData();
+			CSVRuns[] iplCSV = new Gson().fromJson(sortedCSVData, CSVRuns[].class);
+			assertEquals(83.2, iplCSV[0].average);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	
 }
+
+	
+	
